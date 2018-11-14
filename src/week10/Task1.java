fc0ecf0f9b3fc3578577e1a531e8f7eb4b9ce412
wowKimSoHuyn <hadesi1999@gmail.com>
2018-11-14 14:02:51 +0700

    this is message

--- src/week10/Task1.java
+++ src/week10/Task1.java
@@ -1,4 +1,21 @@
 package week10;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
 public class Task1 {
+
+    public List<String> getAllFunctions(File path){
+
+        try {
+            FileInputStream fis = new FileInputStream(new File("src\\week9\\Utils.java"));
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            
+        }catch(Exception ex){}
+        return null;
+    }
 }
--- src/week10/Task2.java
+++ src/week10/Task2.java
@@ -1,4 +1,34 @@
 package week10;
 
+import java.util.Random;
+
 public class Task2 {
+
+    public static void bubbleSort(double[] arr, int size){
+
+        for (int i = 0; i < size - 1; ++i){
+
+            for (int j = 0 ; j < size - i - 1;++j){
+                if (arr[j] > arr[j+1]){
+                    double temp;
+                    temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for (double n : arr){
+            System.out.println(n);
+        }
+    }
+
+    public static void main (String[] args){
+        Random rd = new Random();
+        double[] arr = new double[1000];
+        for (int i = 0; i < 1000; i++){
+            arr[i] = rd.nextDouble()*770;
+        }
+        bubbleSort(arr,1000);
+
+    }
 }
